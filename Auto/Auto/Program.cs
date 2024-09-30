01fca41c93734aca28d9bb09a97cf8824ba854a3
VeruHorena <horenovska.veronika@gmail.com>
2024-09-30 12:05:02 +0200

    hodina 30_9

---
+++ Auto/Auto/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace Auto
+{
+
+    public class DopravniProstredek
+    {
+        public string Jmeno { get; set; }
+
+        public void Jizda()
+        {
+            Console.WriteLine("Dopravní prostředek jede.");
+        }
+    }
+
+    public class Auto : DopravniProstredek
+    {
+        public void ZapniMotor()
+        {
+            Console.WriteLine("Motor je zapnutý.");
+        }
+    }
+
+    public class Kolo : DopravniProstredek
+    {
+        public void Slapej()
+        {
+            Console.WriteLine("Šlapání na kole.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Auto mojeAuto = new Auto();
+            mojeAuto.Jmeno = "BMW";
+            mojeAuto.Jizda();
+            mojeAuto.ZapniMotor();
+
+            Kolo mojeKolo = new Kolo();
+            mojeKolo.Jmeno = "Favorit";
+            mojeKolo.Jizda();
+            mojeKolo.Slapej();
+        }
+    }
+
+}
+
+
---
+++ Zamestnanec/Zamestnanec/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Zamestnanec
+{
+    public class Zamestnanec
+    {
+        public string Jmeno { get; set; }
+
+        public void Pracuj()
+        {
+            Console.WriteLine("Zaměstnanec pracuje.");
+        }
+    }
+
+    public class Ucitel : Zamestnanec
+    {
+        public void Uc()
+        {
+            Console.WriteLine("Učitel učí.");
+        }
+    }
+
+    public class Programator : Zamestnanec
+    {
+        public void Programuj()
+        {
+            Console.WriteLine("Programátor programuje.");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ucitel ucitel = new Ucitel();
+            ucitel.Jmeno = "Jan Novák";
+            ucitel.Pracuj();
+            ucitel.Uc();
+
+            Programator programator = new Programator();
+            programator.Jmeno = "Petr Svoboda";
+            programator.Pracuj();
+            programator.Programuj();
+        }
+    }
+
+}
