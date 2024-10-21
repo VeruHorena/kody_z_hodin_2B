c7b1ad31e71a1aaced476c96d04ee5d1e2557913
VeruHorena <horenovska.veronika@gmail.com>
2024-10-21 12:42:54 +0200

    dictionary

---
+++ dict/dict/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace dict
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int pocet = 0;
+            Dictionary<char, int> tabulkaZnaku = new Dictionary<char, int>();
+            Console.WriteLine("Zadej slovo k analyze:");
+            string slovo = Console.ReadLine();
+            List<char> projiteZnaky = new List<char>();
+            for (int i = 0; i < slovo.Length; i++)
+            {
+                pocet = 0;
+                if (projiteZnaky.Contains(slovo[i]) == false)
+                {
+                    projiteZnaky.Add(slovo[i]);
+                    for (int j = 0; j < slovo.Length; j++)
+                    {
+                        if (slovo[j] == slovo[i])
+                            pocet++;
+
+                    }
+                    tabulkaZnaku.Add(slovo[i], pocet);
+                }
+            }
+            vypisSlovnik(tabulkaZnaku);
+        }
+        public static void vypisSlovnik(Dictionary<char, int> dict)
+        {
+            foreach (char c in dict.Keys)
+            {
+                int hodnota;
+                dict.TryGetValue(c, out hodnota);
+                Console.WriteLine("Znak " + c + " je " + hodnota);
+
+
+            }
+        }
+
+    }
+    
+}
+
